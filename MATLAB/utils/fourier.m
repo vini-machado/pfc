df48887ec7d5785bf3aa5da2f266d80dc5dcaaa7
Vinicius Cordeiro Machado <59898598+vini-machado@users.noreply.github.com>
2023-03-14 12:38:01 -0300

    remove codes folder

--- MATLAB/utils/fourier.m
+++
@@ -1,8 +0,0 @@
-function Fo = fourier(diffusion_coef, delta_t, delta_x)
-%fourier - Description
-%
-% Syntax: Fo = fourier(diffusion_coef, delta_t, delta_x)
-%
-% Long description
-    Fo = diffusion_coef * delta_t / (delta_x^2)
-end
