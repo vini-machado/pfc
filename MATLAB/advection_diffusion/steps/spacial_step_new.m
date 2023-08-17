59eef3427ecb60a9b8f21e0137eb0238f32f88b3
vini-machado <viniciuscmachado22@gmail.com>
2023-08-17 11:39:48 -0300

    remove unuseful files

--- MATLAB/advection_diffusion/steps/spacial_step_new.m
+++
@@ -1,8 +0,0 @@
-function [step_size, number_of_points] = spacial_step_new(~, dimension_number_of_points)
-    d = inputs;
-
-    % step_size: Distância entre pontos da malha (delta_x)  
-    step_size = d.total_distance/(dimension_number_of_points - 1);
-    number_of_points = dimension_number_of_points;
-
-end
