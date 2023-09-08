5feae09d086ade822ab025e16129a34c1e35fa3b
vini-machado <viniciuscmachado22@gmail.com>
2023-09-08 13:53:39 -0300

    including dark mode

--- MATLAB/advection_diffusion/mdf_2D/plot_2D.m
+++ MATLAB/advection_diffusion/mdf_2D/plot_2D.m
@@ -2,24 +2,29 @@ function [] = plot_2D(data_inputs, mp, concentration_per_volume)
     d = data_inputs;
 
     for t = 1:mp.t_number_of_points
-        % h = contourf(mp.x_grid, mp.y_grid, concentration(:,:,t));
         
-        surf(mp.x_grid, mp.y_grid, concentration_per_volume(:,:,t))
-        view(0,50);
+        % h = contourf(mp.x_grid, mp.y_grid, concentration_per_volume(:,:,t), 25);
+        
+        h = surf(mp.x_grid, mp.y_grid, concentration_per_volume(:,:,t));
+
+        set(h,'linestyle','none');
+
+        view(2);
         zlim([0,d.C_x1]);
         xlim([0,d.total_distance]);
         ylim([0,d.total_distance]);
-    
+
         xlabel('Distância (m)'); 
         ylabel('Distância (m)'); 
         zlabel('Concentração (Bq/m³)');
 
         title("Tempo de simulação " + num2str(mp.delta_t*t, 2) + " s");
     
+        colormap(hot)
         colorbar
         clim([0 d.C_x1]);
-        clim([0 d.C_x1]);
-    
-        pause(.05)   
+
+        plot_darkmode
+        pause(.05)
     end
 end
