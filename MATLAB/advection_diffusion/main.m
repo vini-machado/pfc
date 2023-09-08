e662665824ab0b6c65c609f33a526c25d3fd4b13
vini-machado <viniciuscmachado22@gmail.com>
2023-09-08 13:52:44 -0300

    including tic-toc on main file

--- MATLAB/advection_diffusion/main.m
+++ MATLAB/advection_diffusion/main.m
@@ -1,4 +1,4 @@
-clear, close all 
+clf, clear, close all
 
 d = inputs;
 len = length(d.number_of_points_array);
@@ -7,10 +7,16 @@ for i = 1:len
     number_of_points = d.number_of_points_array(i);
     
     if d.spacial_dimensions == 1
+        tic
         [concentration_i, mp] = concentration_1D(d, number_of_points);
+        toc
+
         plot_1D(d, mp, concentration_i);
     else
+        tic
         [concentration_i, mp] = concentration_2D(d, number_of_points);
+        toc
+
         plot_2D(d, mp, concentration_i);
     end
 end
