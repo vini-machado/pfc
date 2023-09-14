e25aaf6a9554ae7536075dd4d0a87cfa14a4ee26
vini-machado <viniciuscmachado22@gmail.com>
2023-09-14 17:59:30 -0300

    plot2D handling concentration > 1

--- MATLAB/advection_diffusion/mdf_2D/plot_2D.m
+++ MATLAB/advection_diffusion/mdf_2D/plot_2D.m
@@ -1,9 +1,10 @@
 function [] = plot_2D(data_inputs, mp, concentration_per_volume, effective_dose)
     d = data_inputs;
     max_dose = max(effective_dose, [], 'all');
+    max_concentration = max(concentration_per_volume, [], 'all');
 
     MIN_DOSE_THRESHOLD = 1E-1;
-    MIN_CONCENTRATION_THRESHOLD = 1E-2;
+    MIN_CONCENTRATION_THRESHOLD = 1E-3;
 
     figure('units','normalized','outerposition',[0 0 1 1])
     for t = 1:mp.t_number_of_points
@@ -31,11 +32,11 @@ function [] = plot_2D(data_inputs, mp, concentration_per_volume, effective_dose)
         colormap(custom_gray)
         cl = colorbar;
 
-        concentration_ticks = colorbar_ticks(MIN_CONCENTRATION_THRESHOLD, d.C_x1);
+        concentration_ticks = colorbar_ticks(MIN_CONCENTRATION_THRESHOLD, max_concentration);
         cl.Ticks = concentration_ticks;
         cl.TickLabels = num2cell(concentration_ticks);
 
-        clim([1E-2 d.C_x1]);
+        clim([MIN_CONCENTRATION_THRESHOLD max_concentration]);
 
         %% Dose Acumulada
 
