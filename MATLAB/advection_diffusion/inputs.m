a0451dc46b3b29be6a386694175772944b43bf86
vini-machado <viniciuscmachado22@gmail.com>
2023-09-14 17:57:41 -0300

    remove wind_field + specify source location

--- MATLAB/advection_diffusion/inputs.m
+++ MATLAB/advection_diffusion/inputs.m
@@ -14,21 +14,14 @@ function data = inputs
     data.permanent_wind_velocity_x = 5;
     data.permanent_wind_velocity_y = 3;
 
-    if data.spacial_dimensions == 1
         wind_field_function_x = @(x, y, t) 1;
         wind_field_function_y = @(x, y, t) 1;
-    else
-        wind_field_function_x = @(x, y, t) sin((y^3))^2;
-        wind_field_function_y = @(x, y, t) cos(sqrt(x))^2;
-    end
 
     data.wind_field_x = @(x, y, t, noise) data.permanent_wind_velocity_x*wind_field_function_x(x, y, t) + noise;
     data.wind_field_y = @(x, y, t, noise) data.permanent_wind_velocity_y*wind_field_function_y(x, y, t) + noise;
 
     %% Parâmetros de estabilidade
-    data.peclet = 20;
     data.courant = 0.5;
-    data.fourier = data.courant/data.peclet;
 
     %% Condições iniciais de concentração
     data.C_t1 = 0;
@@ -36,7 +29,7 @@ function data = inputs
     data.C_y1 = data.C_x1;
 
     %% Localização da fonte
-    data.source_x = 5:5;
+    data.source_x = (data.number_of_points_array/20):(data.number_of_points_array/20);
     data.source_y = data.source_x;
 
     %% Parâmetro estocástico
