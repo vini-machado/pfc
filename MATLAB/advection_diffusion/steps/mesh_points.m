183b69f5fc567a7a0e7cd5064d50c703ca252654
vini-machado <viniciuscmachado22@gmail.com>
2023-09-15 20:35:21 -0300

    [gambiarra] fixing invert xy axis

--- MATLAB/advection_diffusion/steps/mesh_points.m
+++ MATLAB/advection_diffusion/steps/mesh_points.m
@@ -10,7 +10,7 @@ function data = mesh_points(dimension_number_of_points)
 
     x_grid = 0:delta_x:d.total_distance;
     y_grid = 0:delta_y:d.total_distance;
-    [X, Y] = meshgrid(x_grid, y_grid);
+    [Y, X] = meshgrid(x_grid, y_grid);
 
     data.delta_x = delta_x;
     data.delta_y = delta_y;
